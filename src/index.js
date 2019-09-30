1ed13150983d9cd87fe72b838cc9c0d2f6788130
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-29 20:35:26 -0400

    adds reactotron configs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StatusBar, YellowBox } from 'react-native';
 
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 
 YellowBox.ignoreWarnings(['RCTRootView cancelTouches']);
